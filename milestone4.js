d1ee5064466d81dc7de0c31f623efcd364975940
Madhumitha490 <70741829+Madhumitha490@users.noreply.github.com>
2021-03-17 01:33:12 -0700

    Create milestone4.js

---
+++ milestone4.js
@@ -0,0 +1,31 @@
+// IF STATEMENT
+//check num is pos or negative
+ <script>
+   var x=2;
+   if(x>0){
+     console.log("positive");
+   }else{
+     console.log("negative");
+   }
+</script>
+
+//while statement
+//print even number
+<script>
+  var x=1;
+  while(x<=10){
+    if(x%2==0){
+      document.write("</h2>" +x+ "</h2>");
+      x++;
+    }
+ </script>
+    
+    
+//FOR STATEMENT 
+//PRINT MULIPLES OF 5
+    
+<script>
+   for(var x=1;x<=10;x++){
+     document.write("</h2>" +(5*x)+ "</h2>");
+   }
+</script>
